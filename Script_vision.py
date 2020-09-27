c3ecf56091fff2b2ce608d9aa5a97e8dc00180b9
Khan Noor <33965773+azura1003@users.noreply.github.com>
2020-09-27 16:48:55 +0200

    Update Script_vision.py

--- Script_vision.py
+++ Script_vision.py
@@ -1,27 +1,27 @@
-import os
+import os # Ensemble de dictionnaires permettant d'utiliser les fonctionnalites nottament windows.
 import datetime
 import psutil
 import win32serviceutil
    
    
-LOOKUP_SERVICE = 'Audiosrv'
+LOOKUP_SERVICE = 'Audiosrv' #On met dans la variable le nom du service qu'on veut tester.
  
 try:
-    service = psutil.win_service_get(LOOKUP_SERVICE)
+    service = psutil.win_service_get(LOOKUP_SERVICE)  # Le script va chercher le service et s'il n'existe pas va afficher un message
 except psutil.NoSuchProcess:
-    print("service not found")
+    print("service introuvable")
 else:
-    print("service trouvé.")
-    if service.status() == 'runnig':
-        print("service est en cours de fonctionnement")
+    print("service trouvé.")   # Le service est trouvé cependant le if va confirmer s'il est en cours d'excution
+    if service.status() == 'running':
+        print("service est en cours de fonctionnement") # si le statut est egale à running alors le programme s'arrete
     else:
-        print("service est arreté")
+        print("service est arreté") #Si le programme est arreté le script va le relancer
         win32serviceutil.StartService(LOOKUP_SERVICE)
         print("le service a été demarré")
         try:
-            os.mkdir(r'C:\Ariane\Logs_script_vision') # on windows back-slash in path may create problems. use raw string or forward slash
-        except FileExistsError: # catch specifi error consistent with the msg you print
+            os.mkdir(r'C:\Ariane\Logs_script_vision') # Le script va essayer de créer un dossier
+        except FileExistsError: # il va capturer l'erreur ici et afficher le message dossier existant s'il est déja présent
             print("Dossier logs déja existant")
-        finally:
+        finally:   # Pour finir il va inscrire ecrire le message de notre choix ainsi que la date et l'heure si le fichier existe pas il va le créer
             with open(r"C:\Ariane\Logs_script_vision\Visionlogs.txt", "a") as fichier:
-                fichier.write(f"Le fichier vision a été relancé par notre script le :{datetime.datetime.now().ctime()}\n")
+                fichier.write(f"Le fichier vision a été relancé par notre script le :{datetime.datetime.now().ctime()}\n") #saut de ligne à la fin
